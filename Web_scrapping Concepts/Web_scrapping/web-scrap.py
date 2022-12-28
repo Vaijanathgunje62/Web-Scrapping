63b76928fe0d499efa9d9537b3c1a9c8cd08791a
vaijanath <vaijanathgunje62>
2022-12-28 16:10:02 +0530

    Done Changes Again

--- Web_scrapping Concepts/Web_scrapping/web-scrap.py
+++ Web_scrapping Concepts/Web_scrapping/web-scrap.py
@@ -15,8 +15,20 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 home_page = "https://prs.moh.gov.sg/prs/internet/profSearch/main.action?hpe=SPC"
 
+
 # Use urllib to open home page, and BeautifulSoup to parse HTML
 home_page_content = urllib.request.urlopen(home_page)
 home_page_html = BeautifulSoup(home_page_content, 'html.parser')
 print(home_page_html,'@@@@@')
+
+# Setting up options for WebDriver
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+
+# Initiate WebDriver
+driver = webdriver.Chrome(options=options)
+
+# Direct WebDriver to the target home page
+driver.get(home_page)
+driver.switch_to.frame(driver.find_element('msg_main'))
 # Note: Please see Jupyter Notebook to see the HTML returned

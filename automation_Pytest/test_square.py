5eaf5fdc5393be9fcf490830870fa19d391e9863
vaijanath <vaijanathgunje62>
2022-12-28 18:43:18 +0530

    Done Changes

--- automation_Pytest/test_square.py
+++ automation_Pytest/test_square.py
@@ -1,16 +1,17 @@
 import math
+import pytest
+import math
 
-# In Selenium, Asserts are validations or checkpoints for an application
-# Asserts in Selenium validate the automated test cases that help testers understand,
-# if tests have passed or failed.
-
+@pytest.mark.square
 def test_sqrt():
    num = 25
    assert math.sqrt(num) == 5
 
+@pytest.mark.square
 def testsquare():
    num = 7
    assert 7*7 == 40
 
-def testequality():
-   assert 10 == 11
+@pytest.mark.others
+def test_equality():
+    assert 10 == 10
